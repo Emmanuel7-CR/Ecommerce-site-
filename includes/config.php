2876916a31b860fcf6bb9c713b334d8087b45f1f
Emmanuel7-CR <eomoniyi245@gmail.com>
2025-11-12 12:04:34 +0100

    Stop tracking config.php

--- includes/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Paystack API keys
-define('PAYSTACK_PUBLIC_KEY', 'pk_test_895a133ccb04898ade5433f539ec4512ffce2e21');
-define('PAYSTACK_SECRET_KEY', 'sk_test_d6f82f7c3c8632fae248bbe264f7816f0eb6b5fe');
-
-?>
